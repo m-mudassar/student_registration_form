93d1831d927e299eb287e5a03f1c398bd319fb66
Farhan Sakhawat <bc160402723@gmail.com>
2022-02-04 20:00:31 +0500

    added updateValue() and generateStudentID() function

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
+// student class
 class Student{
     studentID = " "
-    constructor(firstName, lastName, studyProgram, semester, year, description){
+    constructor(firstName, lastName, studyProgram, semester, year, about){
         this.firstName = firstName
         this.lastName = lastName
         this.studyProgram = studyProgram
         this.semester = semester
         this.year = year
-        this.description = description
+        this.about = about
     }
 }
 
-var st = new Student("mudassar", "malik", "bsse", "Fall2018")
-console.log(st.firstName) 
-console.log(st.lastName)
-console.log(st.studyProgram) 
+var firstName = " "
+var lastName = " "
+var studyProgram = " "
+var semester = " "
+var year = " "
+var about = " "
+var studenID = " "
+var rollNumber = 0
+
+
+function generateStudentID(studyProgram, year, rollNumber){
+    var firstTwoLettersOfStudyProgram = studyProgram.substring(0 , 2)
+    console.log(firstTwoLettersOfStudyProgram)
+    console.log("calling")
+}
+
+function updateValue() {
+    firstName = document.querySelector(".first-name").value
+    lastName = document.querySelector(".last-name").value
+    studyProgram = document.querySelector(".study-program").value
+    semester = document.querySelector(".semester").value
+    year = document.querySelector(".year").value
+    about = document.querySelector(".about").value
+
+    // console.log("First Name: " + firstName +
+    //  " Last Name: " + lastName +
+    //  " Study Program: " + studyProgram +
+    //  " Semester: " + semester +
+    //  " Year " + year +
+    //  " About " + about)
+    generateStudentID(firstName, year, rollNumber)
+}
+
+
+
+
+
 
